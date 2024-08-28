e9a1f51de0fa6c2a125edf644771ce8d77650792
ronakptl996 <pronak391@gmail.com>
2024-08-28 15:18:07 +0530

    setup backend

---
+++ backend/src/app.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
+
+app.use(express.static("temp"));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+export { app };
