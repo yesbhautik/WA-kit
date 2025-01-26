e96ca55f60b88a3980e3f537eabfd89a14315348
ronakptl996 <pronak391@gmail.com>
2025-01-26 16:32:46 +0530

    fix: build issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BACKEND_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  };
+});
